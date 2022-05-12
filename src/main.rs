c5fe16e82312857e06fb26e440900437941bf204
田口魁人 <tama@taguchiisaonins-Mac-mini.local>
2022-05-12 18:05:30 +0900

    implemented basic feature

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
-type Quizes = Vector<Quiz>;
+use std::io;
+use std::io::BufReader;
+use std::fs::File;
+use std::collections::HashMap;
 
+use serde::{Deserialize, Serialize};
+
+type Quizes = Vec<Quiz>;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct QuizJson {
+    jsonquiz: HashMap <String, HashMap<String, String>>
+}
 struct Quiz {
     question: String,
     answer: String
 }
 impl Quiz {
     fn new(question: &str, answer: &str) -> Quiz {
-        retutn Quiz {
+        let mut quiz = Quiz {
             question: String::from(question),
             answer: String::from(answer),           
         };
+        quiz
     }
-    fn inquire() {
+    fn inquire(&self) {
         println!("{}", self.question);
     }
-    fn judge(x:&str) -> bool {
+    fn judge(&self, x:&str) -> bool {
         if self.answer == x {
             return true;
         }
@@ -22,12 +34,33 @@ impl Quiz {
     }
 }
 fn solve(quizes: Quizes) {
-    for i in quizes.iter() {
-
+    for q in quizes.iter() {
+        println!("問題:{}", q.question);
+        let mut ans = String::new();
+        let stdin = io::stdin();
+        stdin.read_line(&mut ans).ok();
+        let ans: String = ans.trim().parse().unwrap();
+        if q.judge(&ans) {
+            println!("正解です");
+        } else {
+            println!("不正解です。\n正解は{}です", q.answer);
+        } 
     }
 }
-fn main() {
-    let one = Quiz::new("りんごは果物である", "o");
-    println!("Hello, world!");
+fn load_question(file_name: &str) -> Quizes {
+    let file = File::open(file_name).unwrap();
+    let reader = BufReader::new(file);
 
+    let deserialized: QuizJson = serde_json::from_reader(reader).unwrap();
+    let mut quizes: Quizes = Vec::new();
+    for (k, v) in deserialized.jsonquiz.iter() {
+        quizes.push(Quiz::new(&v["question"], &v["answer"]));
+    }
+    quizes
+    
+}
+fn main() {
+    let file_name = "quz.json";
+    let quizes = load_question(file_name);
+    solve(quizes);
 }
