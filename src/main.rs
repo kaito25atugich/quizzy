888ed41eb3963c6ce82c5179bc7c5a833b75b58e
kaito <tl.crustacean@gmail.com>
2022-05-20 22:36:46 +0900

    ver.0.1

--- src/main.rs
+++ src/main.rs
@@ -33,19 +33,58 @@ impl Quiz {
         false
     }
 }
-fn solve(quizes: Quizes) {
-    for q in quizes.iter() {
-        println!("問題:{}", q.question);
+fn solve(quizes: &Quizes) {
+    let all = quizes.len();
+    let mut correct = 0;
+    let mut wrong: Quizes = Vec::new();
+
+    println!("何問解きますか?1以上{}以下。整数で入力してください", all);
+    let mut numofq = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut numofq).ok();
+    let mut numofq: usize = numofq.trim().parse().unwrap();
+
+    if numofq > all {
+        numofq = all;
+    } else if numofq < 1 {
+        numofq = 1;
+    }
+    for (i, q) in quizes.iter().enumerate() {
+        if numofq == i {break};
+        println!("問題{}:{}", i+1, q.question);
         let mut ans = String::new();
         let stdin = io::stdin();
         stdin.read_line(&mut ans).ok();
         let ans: String = ans.trim().parse().unwrap();
         if q.judge(&ans) {
             println!("正解です");
+            correct += 1;
         } else {
             println!("不正解です。\n正解は{}です", q.answer);
+            wrong.push(Quiz::new(&q.question, &q.answer));
         } 
     }
+    println!("{}問中 {}問正解です", numofq, correct);
+    loop {
+        if (wrong.len() > 0) {
+            println!("間違えた問題を復習しますか？(y/n)");
+            let mut ans = String::new();
+            let stdin = io::stdin();
+            stdin.read_line(&mut ans).ok();
+            let ans: String = ans.trim().parse().unwrap();
+            if ans == "y" || ans == "Y" {
+                solve(&wrong);
+                break;
+            } else {
+                println!("bye");
+                break;
+            }
+        } else {
+            break;
+        }
+        
+    }
+    
 }
 fn load_question(file_name: &str) -> Quizes {
     let file = File::open(file_name).unwrap();
@@ -61,6 +100,6 @@ fn load_question(file_name: &str) -> Quizes {
 }
 fn main() {
     let file_name = "quz.json";
-    let quizes = load_question(file_name);
-    solve(quizes);
+    let mut quizes = load_question(file_name);
+    solve(&quizes);
 }
